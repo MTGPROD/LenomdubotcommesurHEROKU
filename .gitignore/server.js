67d19a1ab8ac522201b5e039e825eabd4c9b1396
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-07 21:49:09 +0100

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -7,6 +7,19 @@ client.on('message', message => {
            message.member.setRoles(['553282040011816960'])
            message.channel.send('Vous êtes enregistré 📝')
      } 
+     
+     if(message.content === prefix + 'smash') {
+          const Juge = message.author.id
+          
+          message.channel.send('Le premier qui dit SMASH pourra utiliser la boule <:smash:512000445791731718>')
+          const filter = m => m.content.includes('discord');
+
+          const collector = message.channel.createMessageCollector(filter, { time: 15000 });
+
+          collector.on('collect', m => message.channel.send(`**${m.author}** a attrapé la boule <:smash:512000445791731718>`));
+
+          collector.on('end', collected => message.channel.send(`Il peut désormais utiliser sa capacité Smash`))
+     } 
 })
 
 client.on('ready', () => {
