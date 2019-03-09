2fafc8b642cf7b385738c273e9c437da51582787
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-09 13:46:13 +0100

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -63,8 +63,28 @@ client.on('message', message => {
      } 
 })
 
+client.on('message', message => {
+     var args = message.content.slice(prefix.length).trim().split(' ')
+     
+     if(message.content === prefix + 'perso') {
+          if(args[0] === 'Captain Hype') {
+               const charEmbedHype = new Discord.RichEmbed()
+                    .setAuthor(message.author.username, message.author.displayAvatarURL)
+                    .setTitle('Informations supplémentaires pour Captain Hype')
+                    .setDescription('"Sucite l\'Engouement"')
+                    .addField(`Poids`, `350% (Lourd)`)
+                    .addField(`Attaque verte`, `Canon laser (35%)`, true)
+                    .addField(`Attaque rouge`, `Coup (30%)`, true)
+                    .addField(`Attaque blue`, `HYPE PUNCH (40%) `, true) 
+                    .addField(`Talent`, `**Recharge:** A 1/10 de pouvoir Smash par tour.`)
+                    .addField(`<:smash:512000445791731718>`, `Laser Jauvert (60%)`)
+               message.channel.send(charEmbedHype)
+          } 
+     } 
+})
+
 client.on('ready', () => {
-    client.user.setGame(`Pré-inscription ouvertes: +register 8 Places restantes`)
+    client.user.setGame(`Pré-inscription ouvertes: +register 8 Places restantes`) 
 })
 
 client.login(process.env.TOKEN) 
