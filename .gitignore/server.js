0308fbc14df6c5ab491970118b3270f12ae8972e
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-18 22:17:56 +0100

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -2,33 +2,18 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const prefix = '+';
 
-
-
-
-client.on('ready', () => {
-    client.user.setGame('Veuillez vous inscrire avant Avril | +register')
-    client.user.setStatus('dnd')
-})
-
-client.on('message', message => {
-    let args = message.content.slice(prefix.length).trim().split(' ');
-    let cmd = args.shift().toLowerCase();
-
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
-    
-	var gameData = {
-     game: false
-}
+var gameData = {
+     active: false
+} 
 
 var p1 = {
-		name: message.author.username, 
-		tag: message.author.discriminator,
-		id: message.author.id,
-		avatar: message.author.avatarURL,
-		character: 'Aucun',
-		inscrit: true,
-        pourcent: '0%' 
+		name: 'Rien', 
+		tag: 'Rien',
+		id: 'Rien',
+		avatar: 'Rien',
+		character: 'Rien',
+		inscrit: false,
+                pourcent: '0%' 
     }
     
 var p2 = {
@@ -41,9 +26,29 @@ var p2 = {
         pourcent: '0%'
     }
 	
+
+client.on('ready', () => {
+    client.user.setGame('Veuillez vous inscrire avant Avril | +register')
+    client.user.setStatus('dnd')
+	console.log('Yes my body was ready.')
+	
+})
+
+client.on('message', message => {
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    
+	
+    
+
+
+
     try {
-        delete require.cache[require.resolve(`./cmds/${cmd}.js`)]
-        let commandFile = require(`./cmds/${cmd}.js`)
+        delete require.cache[require.resolve(`./${cmd}.js`)]
+        let commandFile = require(`./${cmd}.js`)
         commandFile.run(client, message, args, gameData, p1,p2);
     } catch (e) {
         console.log(e.stack)
